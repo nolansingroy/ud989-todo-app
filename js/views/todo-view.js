b5c495eed83283e51b79eae28582c7f0dea66c44
duke <nolansingroy>
2018-01-24 21:36:54 -0600

    added a new feature created a priority btn to that turns the uncompleted task to color red, had to create 2 new classes in todo-view.js and another in todo.js, finally a priority-btn in index.html

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -68,7 +68,7 @@ var app = app || {};
 				app.TodoFilter === 'completed';
 		},
 
-    //toggle the priority state of the model
+    //toggle the `"priority"` state of the model
 		togglePriority: function () {
 			this.model.togglePriority();
 		},
